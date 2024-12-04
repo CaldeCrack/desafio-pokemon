888cc15e07c7a8406b011d5783c6e5064d1e6643
CaldeCrack <andres.calderon@ug.uchile.cl>
2024-12-04 02:35:42 -0300

    Add better coloring to bar chart

--- app/components/PokemonInfo.tsx
+++ app/components/PokemonInfo.tsx
@@ -7,7 +7,7 @@ import useSWR from 'swr';
 
 const capitalizePhrase = (str: string) => {
 	if (str === 'ho-oh') return 'Ho-oh';
-	const exceptions: Array<string> = ['porygon-z', 'wo-chien', 'chi-yu', 'chien-pao', 'ting-lu'];
+	const exceptions: string[] = ['porygon-z', 'wo-chien', 'chi-yu', 'chien-pao', 'ting-lu'];
 	const splitter = str in exceptions ? ' ' : '-';
 	const capitalize = (word: string) => {return word.charAt(0).toUpperCase() + word.substring(1);}
 	return str.split('-').map((word: string) => capitalize(word)).join(splitter);
@@ -27,7 +27,7 @@ const formatName = (name: string) => {return capitalizePhrase(name);}
 const formatHeight = (height: number) => {return `${height/10} m`;}
 const formatWeight = (weight: number) => {return `${weight/10} kg`;}
 
-const formatTypes = (types: Array<any>) => {
+const formatTypes = (types: any[]) => {
 	return types.map((typeObject: any) => (
 		<span key={typeObject.type.name} className={`${styles.type} ${styles[typeObject.type.name]}`}></span>
 	));
@@ -61,7 +61,7 @@ const formatAbilities = (ability: any, is_hidden: boolean = false) => {
 	)
 }
 
-const formatGenera = (genera: Array<any>) => {
+const formatGenera = (genera: any[]) => {
 	let genus;
 	for (const elem of genera) {
 		if (elem.language.name === 'es')
@@ -72,9 +72,9 @@ const formatGenera = (genera: Array<any>) => {
 	return genus;
 }
 
-const formatStats = (stats: Array<any>) => {
-	let xData: Array<string> = ['HP', 'Atk', 'Def', 'SpA', 'Spd', 'Spe'];
-	let yData: Array<number> = [];
+const formatStats = (stats: any[]) => {
+	const xData: string[] = ['HP', 'Atk', 'Def', 'SpA', 'Spd', 'Spe'];
+	const yData: number[] = [];
 	stats.forEach((elem: any) => {yData.push(elem.base_stat);});
 	const total = stats.reduce((accum, elem) => {return accum + elem.base_stat}, 0);
 
@@ -83,33 +83,77 @@ const formatStats = (stats: Array<any>) => {
 			<p><u>Total:</u> {total}</p>
 			<BarChart
 				grid={{ horizontal: true }}
+				tooltip={{ trigger: 'none' }}
 				barLabel='value'
+				
 				xAxis={[{
 					id: 'stats',
 					data: xData,
 					scaleType: 'band',
 					disableTicks: true,
+					colorMap: {
+						type: 'ordinal',
+						colors: [
+							'url(#gradientHP)',
+							'url(#gradientAtk)',
+							'url(#gradientDef)',
+							'url(#gradientSpA)',
+							'url(#gradientSpD)',
+							'url(#gradientSpe)'
+						]
+					}
 				}]}
-				series={[{
-					data: yData,
-					type: 'bar'
-				}]}
+				series={[{ data: yData, type: 'bar' }]}
 				slotProps={{
-					loadingOverlay: { message: 'Loading data' },
-					noDataOverlay: { message: 'There is no data available' },
+					loadingOverlay: { message: 'Cargando información' },
+					noDataOverlay: { message: 'No hay información disponible' },
 				}}
 				sx={{
 					'& .MuiChartsAxis-root .MuiChartsAxis-tickLabel': {fill: 'white'},
 					'& .MuiChartsAxis-root .MuiChartsAxis-line': {stroke: 'white'},
 					'& .MuiChartsAxis-left .MuiChartsAxis-tick': {stroke: 'white'},
 					'.MuiChartsGrid-line': {stroke: 'gray'},
-				}}
-			/>
+					'.MuiBarElement-root': {strokeWidth: 2},
+					'.MuiBarElement-root:nth-of-type(1)': {stroke: '#448f0c'},
+					'.MuiBarElement-root:nth-of-type(2)': {stroke: '#9b8510'},
+					'.MuiBarElement-root:nth-of-type(3)': {stroke: '#97410c'},
+					'.MuiBarElement-root:nth-of-type(4)': {stroke: '#0d7f9d'},
+					'.MuiBarElement-root:nth-of-type(5)': {stroke: '#304591'},
+					'.MuiBarElement-root:nth-of-type(6)': {stroke: '#8b1370'},
+					'.MuiBarLabel-root': {fontWeight: 1000},
+				}}>
+				<defs>
+					<linearGradient id="gradientHP" x2="0" y2="1">
+						<stop offset="0%" stopColor="#69dc12" />
+						<stop offset="100%" stopColor="#a2db76" />
+					</linearGradient>
+					<linearGradient id="gradientAtk" x2="0" y2="1">
+						<stop offset="0%" stopColor="#efcc18" />
+						<stop offset="100%" stopColor="#edda80" />
+					</linearGradient>
+					<linearGradient id="gradientDef" x2="0" y2="1">
+						<stop offset="0%" stopColor="#e86412" />
+						<stop offset="100%" stopColor="#e5a27b" />
+					</linearGradient>
+					<linearGradient id="gradientSpA" x2="0" y2="1">
+						<stop offset="0%" stopColor="#14c3f1" />
+						<stop offset="100%" stopColor="#81d9ef" />
+					</linearGradient>
+					<linearGradient id="gradientSpD" x2="0" y2="1">
+						<stop offset="0%" stopColor="#4a6adf" />
+						<stop offset="100%" stopColor="#778ddd" />
+					</linearGradient>
+					<linearGradient id="gradientSpe" x2="0" y2="1">
+						<stop offset="0%" stopColor="#d51dad" />
+						<stop offset="100%" stopColor="#d372bc" />
+					</linearGradient>
+				</defs>
+			</BarChart>
 		</div>
 	)
 }
 
-const formatDescription = (entries: Array<any>) => {
+const formatDescription = (entries: any[]) => {
 	let description;
 	for (const elem of entries) {
 		if (elem.language.name === 'es')
@@ -133,10 +177,11 @@ export default function PokemonInfo(pokemon: any) {
 	const shinyImageURL = info.sprites.front_shiny;
 
 	const [isShiny, setIsShiny] = useState(false);
+	const [spriteLoaded, setSpriteLoaded] = useState(false);
 	const currentImageURL = isShiny ? shinyImageURL : imageURL;
 
 	// Handle abilities info
-	let abilitiesInfo: Array<any> = [];
+	const abilitiesInfo: any[] = [];
 	info.abilities.forEach((ability: any) => {
 		const { data, isLoading } = useSWR(`/api/ability?name=${ability.ability.name}`, fetchData);
 		if (isLoading) return <div>Cargando...</div>;
@@ -150,7 +195,10 @@ export default function PokemonInfo(pokemon: any) {
 	const { data, isLoading } = useSWR(`/api/pokemon-species?id=${info.species.name}`, fetchData);
 
 	// Canvas behavior
-	const handleCanvasClick = () => {setIsShiny((prev) => !prev);};
+	const handleCanvasClick = () => {
+		setSpriteLoaded(false);
+		setIsShiny((prev) => !prev);
+	};
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -165,6 +213,7 @@ export default function PokemonInfo(pokemon: any) {
 		image.onload = () => {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			context.drawImage(image, 0, 0, canvas.width, canvas.height);
+			setSpriteLoaded(true);
 		};
 	}, [data, isLoading, currentImageURL, abilitiesInfo]);
 
@@ -187,8 +236,7 @@ export default function PokemonInfo(pokemon: any) {
 								fontSize: '0.9em',
 							}
 						}
-					}}
-				>
+					}}>
 					<div className='w-fit'>
 						<h1 className="text-xl"><strong>{name} #{id}</strong></h1>
 						<p className="text-sm">{genus}</p>
@@ -199,7 +247,7 @@ export default function PokemonInfo(pokemon: any) {
 					<canvas
 						onClick={handleCanvasClick}
 						ref={canvasRef}
-						className={`${styles.render} ${isShiny ? styles.isShiny : ''}`}
+						className={`${styles.render} ${isShiny && spriteLoaded ? styles.isShiny : ''}`}
 					></canvas>
 				</div>
 				<div>
